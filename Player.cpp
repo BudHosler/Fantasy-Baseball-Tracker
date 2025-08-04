9a24cf3b0ebb75eb4f2fa2ab7c937035455d94db
Gene Hosler <bhosler89@gmail.com>
2025-08-04 12:38:24 -0400

    Add project files.

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+#include "Player.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Player::setName(string inputName)
+{
+	name = inputName;
+}
+
+void Player::setTeam(string inputTeam)
+{
+	team = inputTeam;
+}
+
---
+++ Player.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+	string name;
+	int team, points;
+
+public:
+	void setName(string);
+	void setTeam(string);
+	void setPoints(int, int, int, int)
+};
