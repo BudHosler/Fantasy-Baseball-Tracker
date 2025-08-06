804a3a89b25aab5c62dc523255e6959deaac7053
Gene Hosler <bhosler89@gmail.com>
2025-08-06 00:36:02 -0400

    Completed hitter csv read

--- Main.cpp
+++ Main.cpp
@@ -14,13 +14,17 @@
 using namespace std;
 
 
-void inputHitterCSV();
+void inputHittersCSV(vector<Player*>&);
+
+void inputPitchersCSV(vector<Player*>&);
 
 int main() {
 
 	
 
 	vector <Player*> players;
+
+	inputHittersCSV(players);
 
 	
 
@@ -41,7 +45,7 @@ int main() {
 	return 0;
 }
 
-void inputHitterCSV()
+void inputHittersCSV(vector<Player*>& player)
 {
 	ifstream fin;
 
@@ -53,49 +57,69 @@ void inputHitterCSV()
 	//skip file header
 	getline(fin, stringInput);
 
-	while(getline(fin, stringInput, ',')) {
-		Hitter hitterEntry;
+	while (getline(fin, stringInput, ',')) {
 
-		hitterEntry.setName(stringInput);
+		Hitter* hitterEntry = new Hitter;
+
+		hitterEntry->setName(stringInput);
 
 		getline(fin, stringInput, ',');
-		hitterEntry.setTeam(stringInput);
+		hitterEntry->setTeam(stringInput);
 
 		fin >> statInput;
-		hitterEntry.setGames(statInput);
+		hitterEntry->setGames(statInput);
 
 		fin >> statInput;
-		hitterEntry.setSingles(statInput);
+		hitterEntry->setSingles(statInput);
 
 		fin >> statInput;
-		hitterEntry.setDoubles(statInput);
+		hitterEntry->setDoubles(statInput);
 
 		fin >> statInput;
-		hitterEntry.setTriples(statInput);
+		hitterEntry->setTriples(statInput);
 
 		fin >> statInput;
-		hitterEntry.setHomeRuns(statInput);
+		hitterEntry->setHomeRuns(statInput);
 
 		fin >> statInput;
-		hitterEntry.setRuns(statInput);
+		hitterEntry->setRuns(statInput);
 
 		fin >> statInput;
-		hitterEntry.setRBI(statInput);
+		hitterEntry->setRBI(statInput);
 
 		fin >> statInput;
-		hitterEntry.setWalks(statInput);
+		hitterEntry->setWalks(statInput);
 
 		fin >> statInput;
-		hitterEntry.setStrikeOuts(statInput);
+		hitterEntry->setStrikeOuts(statInput);
 
 		fin >> statInput;
-		hitterEntry.setHBP(statInput);
+		hitterEntry->setHBP(statInput);
 
 		fin >> statInput;
-		hitterEntry.setStolenBases(statInput);
+		hitterEntry->setStolenBases(statInput);
 
 		fin >> statInput;
-		hitterEntry.setCaughtStealing(statInput);
+		hitterEntry->setCaughtStealing(statInput);
 
-		players.push_back(&hitterEntry);
+		player.push_back(hitterEntry);
+	}
+	fin.close();
+}
+
+void inputPitcherCSV(vector<Player*>& player) {
+	ifstream fin;
+
+	fin.open("pitchers.csv");
+
+	string stringInput;
+	int statInput;
+
+	//skip file header
+	getline(fin, stringInput);
+
+	while (getline(fin, stringInput, ',')) {
+		Pitcher* pitcherEntry
+	}
+
 }
